2689457a63ac0327bf67f536294e77288d2f0399
Anish <administrator@Anishs-MacBook-Pro.local>
2024-05-15 17:44:12 -0400

    Week updates

---
+++ week2/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <?php
+    ?>
+    <?php 
+    
+    $fname = "Anish";
+    $lname = "Patel";
+    echo "PHP START
+        Hello $fname $lname
+        <h1>Welcome to HTTP 5225 . $fname $lname</h1>
+        <p>I am learning PHP</p>
+        <ul>
+            <li>PHP</li>
+            <li>MYSQL</li>
+            <li>LARAVEL</li>
+        </ul>
+        
+        PHP END"
+    ?>
+
+    <h1>Welcome to HTTP 5225</h1>
+    <p>I am learning PHP</p>
+    <ul>
+        <li>PHP</li>
+        <li>MYSQL</li>
+        <li>LARAVEL</li>
+    </ul>
+    <img src="https://placehold.co/600x400" alt="Placeholder">
+</body>
+
+</html>
