be135671c123a37fc7d20df5189f24daa36c2aa8
Anish <administrator@Anishs-MacBook-Pro.local>
2024-06-12 18:03:34 -0400

    week Updates

---
+++ week6/inc/updateSchool.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['updateSchool'])) {
+    // Connection string
+    require('../reusable/con.php');
+    // print_r($_POST);
+    $id = $_POST['id'];
+    $schoolName = $_POST['schoolName'];
+    $schoolLevel = $_POST['schoolLevel'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+
+    $query = "UPDATE `schools` SET `School Name`='$schoolName',`School Level`='$schoolLevel',`Phone`='$phone',`Email`='$email' WHERE `id`='$id'";
+
+    $school = mysqli_query($connect, $query);
+
+    if ($school) {
+        header("Location: ../index.php");
+    } else {
+        echo "Failed: " . mysqli_error($connect);
+    }
+} else {
+    echo "Not Authorized";
+}
