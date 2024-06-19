d29301261c6da2a8edaabaa03948c420f261009f
Anish <administrator@Anishs-MacBook-Pro.local>
2024-06-19 17:42:00 -0400

    Week 7

--- week7/inc/addSchool.php
+++ week7/inc/addSchool.php
@@ -1,4 +1,6 @@
 <?php
+
+    include('functions.php');
     if (isset($_POST['addSchool'])) {
         // print_r($_POST);
         $schoolName = $_POST['schoolName'];
@@ -13,6 +15,7 @@
         $school = mysqli_query($connect, $query);
 
         if ($school) {
+            set_message('School was added sucessfully!', 'success');
             header("Location: ../index.php");
         } else {
             echo "Failed: " . mysqli_error($connect);
